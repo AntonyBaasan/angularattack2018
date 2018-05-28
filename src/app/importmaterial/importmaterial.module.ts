c93cd1f397be94ed74fa66dcace7a1938db4485d
Antony Baasandorj <antony.baasan@gmail.com>
2018-05-28 15:16:53 -0400

    add mat-icons

--- src/app/importmaterial/importmaterial.module.ts
+++ src/app/importmaterial/importmaterial.module.ts
@@ -14,6 +14,7 @@ import {
   MatProgressSpinnerModule,
   MatSortModule,
   MatExpansionModule,
+  MatIconModule,
 } from '@angular/material';
 
 const modules = [
@@ -31,6 +32,7 @@ const modules = [
   MatProgressSpinnerModule,
   MatSortModule,
   MatExpansionModule,
+  MatIconModule,
 ];
 
 @NgModule({
